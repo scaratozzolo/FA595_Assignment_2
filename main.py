0277e0a423252e0675e329de155b5eec7cac74b4
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-10-12 08:25:00 -0400

    moved to function

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-pairs = pd.DataFrame({"Company_Name":[], "Company_Purpose":[]})
 
-i = 0
-while i < 50:
+def create_company_pairs(num_pairs=50):
+    pairs = pd.DataFrame({"Company_Name":[], "Company_Purpose":[]})
 
-    try:
-        r = requests.get("http://3.85.131.173:8000/random_company")
-    except Exception as e:
-        print(e)
-        print("request failed")
+    i = 0
+    while i < num_pairs:
 
-    soup = BeautifulSoup(r.content, "lxml")
-    lis = soup.find_all("li")
-    company_info = {i.decode_contents().split(":")[0].strip():i.decode_contents().split(":")[1].strip() for i in lis}
+        try:
+            r = requests.get("http://3.85.131.173:8000/random_company")
+        except Exception as e:
+            print(e)
+            print("request failed")
 
-    pairs = pairs.append(pd.DataFrame({"Company_Name":[company_info["Name"]], "Company_Purpose":[company_info["Purpose"]]}), ignore_index=True)
+        soup = BeautifulSoup(r.content, "lxml")
+        lis = soup.find_all("li")
+        company_info = {i.decode_contents().split(":")[0].strip():i.decode_contents().split(":")[1].strip() for i in lis}
 
-    i += 1
+        pairs = pairs.append(pd.DataFrame({"Company_Name":[company_info["Name"]], "Company_Purpose":[company_info["Purpose"]]}), ignore_index=True)
 
-print(pairs)
-pairs.to_csv("name_purpose_pairs.csv", index=False)
+        i += 1
+
+    pairs.to_csv("name_purpose_pairs.csv", index=False)
+    return pairs
